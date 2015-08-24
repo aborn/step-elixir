fc69e9807557760e0a78b29f67e9d41d8140fcc5
guobao.jiang <loveaborn@foxmail.com>
2015-08-24 23:37:15 +0800

    defmodule example

--- hello.ex
+++ hello.ex
@@ -1,1 +1,15 @@
+## hello word
 IO.puts "Hello world"
+
+strA = "abcd ef k中国kkk"
+strB = "abcd efabc"
+
+defmodule StringUtils do
+  def diff(strA, strB) do
+    #    IO.puts Enum.map(String.codepoints(strA), fn x -> x <> "1" end)
+    IO.puts String.codepoints(strA) -- String.codepoints(strB)
+  end
+end
+
+StringUtils.diff(strA, strB)
+
