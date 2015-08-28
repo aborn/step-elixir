4c8bae19abef7bdec6ebc0b5515c68d8a426e602
guobao.jiang <loveaborn@foxmail.com>
2015-08-28 23:52:56 +0800

    StringUtils

--- hello.ex
+++ hello.ex
@@ -1,13 +1,31 @@
 ## hello word
 IO.puts "Hello world"
 
-strA = "abcd ef k中国kkk"
-strB = "abcd efabc"
+strA = "abcd 中ef k中国kkk"
+strB = "abcdkkk 中在efabc"
 
 defmodule StringUtils do
   def diff(strA, strB) do
     #    IO.puts Enum.map(String.codepoints(strA), fn x -> x <> "1" end)
-    IO.puts String.codepoints(strA) -- String.codepoints(strB)
+    #    IO.puts String.codepoints(strA) -- String.codepoints(strB)
+    IO.puts "长字符串:" <> maxString(strA, strB)
+    IO.puts "短字符串:" <> minString(strA, strB)
+  end
+  
+  def maxString(strA, strB) do
+    if(String.length(strA) > String.length(strB)) do
+      strA
+    else
+      strB
+    end
+  end
+
+  def minString(strA, strB) do
+    if(String.length(strA) < String.length(strB)) do
+      strA
+    else
+      strB
+    end
   end
 end
 
