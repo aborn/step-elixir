d0970c2f3a2148d8102417ee0efe74ba354d0fc4
guobao.jiang <loveaborn@foxmail.com>
2015-12-12 22:22:17 +0800

    add doc

---
+++ doc/dist/sidebar_items.js
@@ -0,0 +1,13 @@
+sidebarNodes = {
+  "modules": [
+    {"id": "EnvUtils",
+"docs": ["getCurrentEnv/0","isBetaEnv/0","isProductionEnv/0"]
+},{"id": "StringUtils",
+"docs": ["diff/2","isBlank/1","onlyEnglish/1","takePrefix/2"]
+},{"id": "VersionUtil",
+"docs": ["compare/2","valid?/1","validate/1"]
+
+  }]
+
+};
+fillSidebarWithNodes(sidebarNodes);
